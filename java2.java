8a15c292db4fa5d365da0294e7084b1627a42cac
Rituparna Debnath <111441612+ritu474@users.noreply.github.com>
2022-08-24 11:14:43 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class java2{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b =sc.nextInt();
+        int a = sc.nextInt();
+        int c=sc.nextInt();
+         sc.close();
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        
+        // Complete this line
+        // Complete this line
+    }
+}
